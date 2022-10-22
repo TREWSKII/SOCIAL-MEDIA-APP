d373de7284eb2a770867987c03f0ceab2a8ce335
Drew <dlederman69@gmail.com>
2022-10-22 16:08:13 -0600

    working on models

---
+++ Models/Thought.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const reaction = require("./Reaction");
+// ! build schema for thought model
+
+const thought = new mongoose.Schema({
+  thoughtText: { type: String, required: true, minlength: 1, maxlength: 280 },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => timestamp.toLocaleString(),
+  },
+  username: { type: String, required: true },
+  reactions: [reaction],
+},
+{toJSON: { virtuals: true, getters: true},  id: false}
+);
+
+// not in reactions
+thought.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+})
+
+// not in reaction going to stay a schema 
+const Thought = model('Thought', thought);
+
+module.export = Thought;
---
+++ Models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+// ! build schema for user model
+
+const user = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true, trim: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, "Must match an email address!"],
+    },
+    thoughts: [{ type: mongoose.Schema.Types.ObjectId, ref: "Thought" }],
+    friends: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  },
+  { toJSON: { virtuals: true }, id: false }
+);
+
+// Create a virtual property `commentCount` that gets the amount of comments per post
+user.virtual('friendCount').get(function () {
+    return this.friends.length;
+  });
+  
+  // Initialize our Post model
+  const User = model('User', user);
+  
+  module.exports = User;
+  
